8caeb2f8a709a56fb4e2e2b186730f7b0eb87c1b
Steve Williams <steve@symless.com>
2018-04-25 13:49:22 +0100

    Initial issue tracker

---
+++ github-issue-trends.py
@@ -0,0 +1,42 @@
+#/usr/bin/env python
+
+import requests
+import json
+from pprint import pprint
+import configparser
+
+if __name__ == '__main__':
+
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    page = 1
+    items_returned = 100
+
+    while items_returned == int(config['github']['items_per_page']):
+
+
+
+        r = requests.get( 'https://api.github.com/search/issues?page={}&per_page={}&q={}'.format(page,
+                                                                                                config['github']['items_per_page'],
+                                                                                                config['github']['query']),
+                          auth=(config['github']['user'], config['github']['password']))
+        print("Status code {}, page {} items_per_page {}".format(r.status_code, page, config['github']['items_per_page']))
+
+
+
+        if r.status_code == 200:
+            page+=1
+
+            github_response = json.loads(r.text)
+
+            print("items {}".format(len(github_response['items'])))
+            items_returned = len(github_response['items'])
+
+            for item in github_response['items']:
+
+                print("{}\n".format(item))
+
+
+        else:
+            items_returned = 0
