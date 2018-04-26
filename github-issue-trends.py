c3a2b633eacd0cff0365630b85f2e35ff0007f98
Steve Williams <steve@symless.com>
2018-04-26 18:01:50 +0100

    Add some anlysis

--- github-issue-trends.py
+++ github-issue-trends.py
@@ -4,7 +4,7 @@ import requests
 import json
 from pprint import pprint
 import configparser
-
+from datetime import datetime, timedelta
 
 if __name__ == '__main__':
 
@@ -13,6 +13,41 @@ if __name__ == '__main__':
 
     page = 1
     items_returned = 100
+
+    issues = []
+
+    weeks = [
+        "01/10/2017",
+        "08/10/2017",
+        "15/10/2017",
+        "22/10/2017",
+        "29/10/2017",
+        "05/11/2017",
+        "12/11/2017",
+        "19/11/2017",
+        "26/11/2017",
+        "03/12/2017",
+        "10/12/2017",
+        "17/12/2017",
+        "24/12/2017",
+        "31/12/2017",
+        "07/01/2018",
+        "14/01/2018",
+        "21/01/2018",
+        "28/01/2018",
+        "05/02/2018",
+        "12/02/2018",
+        "19/02/2018",
+        "26/02/2018",
+        "05/03/2018",
+        "12/03/2018",
+        "19/03/2018",
+        "26/03/2018",
+        "02/04/2018",
+        "09/04/2018",
+        "16/04/2018",
+        "23/04/2018",
+        ]
 
     while items_returned == int(config['github']['items_per_page']):
 
@@ -41,11 +76,58 @@ if __name__ == '__main__':
                     if lab['name'] in ['bug', 'feature','enhancement']:
                         type = lab['name']
 
-                print("#{}\t{}\t{}\t{}".format(item['number'],
-                                                type,
-                                                item['created_at'],
-                                                item['closed_at']))
+                #print("#{}\t{}\t{}\t{}".format(item['number'],
+                 #                               type,
+                  #                              item['created_at'],
+                   #                             item['closed_at']))
+
+                issues.append(item)
 
 
         else:
             items_returned = 0
+
+
+    count_dict = {}
+
+    for week in weeks:
+        week_ending = datetime.strptime(week,"%d/%m/%Y")
+        week_start = week_ending - timedelta(days=7)
+        print ("{} {}".format(week_start,week_ending))
+
+        count_dict[week] = {'bug':0, 'feature':0, 'enhancement':0,'unknown':0}
+
+        for issue in issues:
+            type = 'unknown'
+            for lab in issue['labels']:
+                if lab['name'] in ['bug', 'feature', 'enhancement']:
+                    type = lab['name']
+
+
+            issue_open = datetime.strptime(issue['created_at'],"%Y-%m-%dT%H:%M:%SZ")
+            if issue['closed_at'] != None:
+                issue_closed = datetime.strptime(issue['closed_at'],"%Y-%m-%dT%H:%M:%SZ")
+            else:
+                issue_closed = None
+
+
+
+            print("\t{}\t{}\t{}\t{}".format(issue['number'], type, issue_open, issue_closed))
+
+
+            if issue_open > week_ending:
+                # not open in this week, so skip
+                continue
+            else:
+                if issue_closed != None and issue_closed < week_start:
+                    #not in this week
+                    continue
+                else:
+                    count_dict[week][type] += 1
+
+
+    print (count_dict)
+
+
+    for key, val in count_dict.items():
+        print("{}\t{}\t{}".format(key, val['bug'], val['enhancement']))
